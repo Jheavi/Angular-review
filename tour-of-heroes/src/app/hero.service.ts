16f81d49b750a403d2cfed2e36a78e84850767b5
Jheavi <javierlaso213@hotmail.com>
2021-04-08 14:01:51 +0200

    add getHero method in heroService

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -16,4 +16,10 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes')
     return heroes
   }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(heroToFind => heroToFind.id === id)
+    this.messageService.add(`HeroService: fetched hero with id: ${id}`)
+    return of(hero)
+  }
 }
