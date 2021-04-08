68f7927bb6b16929f60d6613274cc3350c901d54
Jheavi <javierlaso213@hotmail.com>
2021-04-08 12:47:59 +0200

    add message when hero is clicked

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
-import { HEROES } from "../mock-heroes";
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,26 +12,22 @@ export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-
   heroes: Hero[] = []
 
-  constructor(private heroService: HeroService) { }
-
-    getHeroes(): void {
-      this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes)
-    }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes()
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero with id: ${hero.id}`)
   }
 
 }
