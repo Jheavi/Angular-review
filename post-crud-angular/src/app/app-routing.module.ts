9144fd2ad278fc17b3bb551967a87b9f225f49e9
Jheavi <javierlaso213@hotmail.com>
2021-04-11 15:11:49 +0200

    add general route

--- post-crud-angular/src/app/app-routing.module.ts
+++ post-crud-angular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsListComponent } from "./posts-list/posts-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: 'list', component: PostsListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
